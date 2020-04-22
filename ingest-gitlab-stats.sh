2738af1810709d53b6cd16cc3feba278d50769bd
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2020-04-22 12:53:32 +0200

    Remove ansi colors from cron task script

--- ingest-gitlab-stats.sh
+++ ingest-gitlab-stats.sh
@@ -3,26 +3,30 @@
 # Script for cron job
 #
 # Crontab schedule line example:
-# 10 * * * *       $PATH/ingest-gitlab-stats.sh 2>&1
+# 10 * * * *       $PATH/ingest-gitlab-stats.sh ~/Developer/gitlab-pipeline-stats 2>&1
 #
+
+export PATH="/usr/local/bin:$PATH"
+
+DOCKER_COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 
 set -e
 set -x
 
-cd ~/Developer/gitlab-pipeline-stats
+cd "$1"
 
 # Save initial ES running status
 set +e
-/usr/local/bin/docker-compose ps elasticsearch | grep elasticsearch | grep ' Up '
+${DOCKER_COMPOSE} ps elasticsearch | grep elasticsearch | grep ' Up '
 es_status=$?
 set -e
 
 # Ingest Gitlab pipelines status
-/usr/local/bin/docker-compose run --rm ingest now
+${DOCKER_COMPOSE} run --rm ingest now
 
 # Stop ES if it was not running and Kibana is not running now
 set +e
-/usr/local/bin/docker-compose ps kibana | grep kibana | grep ' Up '
+${DOCKER_COMPOSE} ps kibana | grep kibana | grep ' Up '
 kibana_status=$?
 set -e
 
@@ -31,5 +35,5 @@ then
     echo "ES was running before or Kibana is running now, leaving ES running"
 else
     echo "ES was not running before and Kibana is not running now, stopping ES..."
-    /usr/local/bin/docker-compose stop
+    ${DOCKER_COMPOSE} stop
 fi
